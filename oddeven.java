e3ba3e2efa23fd001a6b310882ed9ac482e4911c
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-12 10:15:41 +0530

    Update oddeven.java

--- oddeven.java
+++ oddeven.java
@@ -1,9 +1,13 @@
 public static void main (String[] args) throws java.lang.Exception
 	{
         int num=5;
+	if(num>0 && num<=100000){
         if(num%2==0){
         	System.out.println("EVEN");
         }else{
         	System.out.println("ODD");
         }
+	}else{
+	System.out.println("INVALID");
+	}
 		}
