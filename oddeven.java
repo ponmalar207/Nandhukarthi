8cc3be9b595d96ae83d0e8732c63afc88a0529e4
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-12 10:10:11 +0530

    Create oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,9 @@
+public static void main (String[] args) throws java.lang.Exception
+	{
+        int num=5;
+        if(num%2==0){
+        	System.out.println("EVEN");
+        }else{
+        	System.out.println("ODD");
+        }
+		}
