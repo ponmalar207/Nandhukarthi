90671d7b8008a3be8192a2f7d69f368c957177a0
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-12 12:13:43 +0530

    Create large.java

---
+++ large.java
@@ -0,0 +1,10 @@
+public static void main(String[] args){
+	int a=-9,b=-6,c=1;
+	if(a>b && a>c){
+		System.out.println(a);
+	}else if(b>a && b>c){
+		System.out.println(b);
+	}else{
+		System.out.println(c);
+	}
+}
