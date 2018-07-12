713bf66ba6fbda398c2bdc5cdbbe7b58ca4743ee
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-12 16:08:33 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,10 @@
+public static void main(String[] args) {
+		int n=5;
+		int k=5,sum=0;
+		int a[]=new int[n];
+		a[0]=1;a[1]=2;a[2]=3;a[3]=4;a[4]=5;
+		for(int i=0;i<k;i++){
+			sum=sum+a[i];
+		}
+System.out.println(sum);
+	}
