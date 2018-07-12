d5f0f79900388a3b2981b89d94e1b8ef6430621d
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-12 12:10:25 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,8 @@
+public static void main(String[] args){
+	 char ch='?';
+	 if((ch>=65 && ch<=90)||(ch>=97 && ch<=122)){
+			 System.out.println("ALPHABET");
+	 }else{
+		 System.out.println("NOT ALPHABET");
+	 }
+ }
